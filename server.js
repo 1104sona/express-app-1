edadfc37d9c9ce571af894f0c6d9b52048455653
1104sona <sonali.pnd04@gmail.com>
2024-09-12 15:55:15 +0530

    finished project

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+//Module to import Database Users
+import userRoutes from './routes/users.js'
+
+const app = express();
+const PORT = 5000
+
+app.use(bodyParser.json());
+
+app.get("/",(req,res) => {
+    console.log('[GET ROUTE]')
+    res.send("<h1>Hello from Homepage.</h1>")
+})
+//Make all users available at the route path
+app.use('/users',userRoutes);
+
+    app.listen(PORT,() => { 
+        console.log(`Server running on port: http://localhost:${PORT}`)
+    })
